0afecdaf8d73568ff257fc42e3378ed46adaa019
Ben <cbtsao47@gmail.com>
2019-08-27 01:16:00 -0500

    update button

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -119,9 +119,9 @@ class App extends React.Component {
         }}
       >
         <ButtonBar />
-        <GameScreen messages={printWords} />
+        {/* <GameScreen messages={printWords} /> */}
         <button onClick={this.initGame}>initial</button>
-        <button onClick={this.move}>move</button>
+        <button onClick={this.explore}>explore</button>
       </div>
     );
   }
